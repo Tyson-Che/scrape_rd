a199a2a8e7828a26665d340c88c5b3a58bd25540
Tyson Che <tysonche@Tysons-Mac-mini.local>
2023-10-15 18:17:38 +0800

    test

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ import logging
 import csv
 from tqdm import tqdm  # Import the tqdm package for progress bars
 from pymongo import MongoClient
-#@todo divide the todos.csv into several pieces
+# @todo divide the todos.csv into several pieces
+# @body 6 leagues, 12 csvs
 
 # Initialize logging
 logging.basicConfig(filename='main.log', level=logging.INFO)
