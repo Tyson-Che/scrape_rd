bd2a3593263f038c87737246ebe7a289e833aa33
Erika Santos <138589728+JocelynConnor953331@users.noreply.github.com>
2023-10-16 16:08:04 +0000

    Initial commit for proxies_pool branch

--- init_config.py
+++ init_config.py
@@ -8,20 +8,28 @@ def init_mongodb():
     # Connect to the MongoDB database
     client = MongoClient("mongodb+srv://mac_m1:damnit@serverlessinstance0.2w1ndw2.mongodb.net/")
 
-def init_reddit_clients():
-    # Initialize Reddit clients
+
+from requests import Session
+
+def init_reddit_clients(proxy_list):
     reddit_clients = []
     user_agent = "Moin"
-
+    
     with open('rd_threads_all.json', 'r') as f:
         all_creds = json.load(f)['credentials']
-
-    for cred in all_creds:
+        
+    for idx, cred in enumerate(all_creds):
+        session = Session()
+        session.proxies = {
+            'http': proxy_list[idx % len(proxy_list)]['http'],
+            'https': proxy_list[idx % len(proxy_list)]['https']
+        }
         reddit = praw.Reddit(
             client_id=cred['client_id'],
             client_secret=cred['client_secret'],
-            user_agent=user_agent
+            user_agent=user_agent,
+            requestor_kwargs={"session": session}  # pass the custom Session instance
         )
         reddit_clients.append(reddit)
-
+    
     return reddit_clients
--- main.py
+++ main.py
@@ -28,11 +28,24 @@ def read_csv_tasks(file_path):
                 'post_id': row[2]
             })
     return tasks
-
+proxy_set = [
+{
+        'http': 'http://13.213.34.125:1029',
+        'https': 'http://13.213.34.125:1029'
+},
+{
+    'http': 'http://4.194.160.73:1029',
+    'https':'http://4.194.160.73:1029'
+}    
+,{
+    'http': 'http://4.193.121.203:1029',
+    'https':'http://4.193.121.203:1029'
+}    
+]
 def main(csv_file_path, limit=None):
     # Initialize MongoDB and Reddit clients
     client = MongoClient("mongodb+srv://mac_m1:damnit@serverlessinstance0.2w1ndw2.mongodb.net/")
-    reddit_clients = init_reddit_clients()
+    reddit_clients = init_reddit_clients(proxy_set)
     
     # Read tasks specific to this server from the given csv_file_path
     tasks = read_csv_tasks(csv_file_path)
@@ -70,5 +83,5 @@ def main(csv_file_path, limit=None):
                 logging.info(f"Successfully processed task {task}")
 
 if __name__ == "__main__":
-    # Run with only the first 10 tasks for the pilot test
-    main('Server_11_todos.csv')
+    # Run with only the first 5 tasks for the pilot test
+    main('todos_chunk_1.csv',limit=5)
